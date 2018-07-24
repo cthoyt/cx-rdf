7e5d487fdd81f329ab065910b15aeb5b49dc6945
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-07-24 16:01:53 +0200

    Update variable name

--- tests/test_export.py
+++ tests/test_export.py
@@ -20,46 +20,46 @@ class TestExport(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """Set up nice CX network and JSON."""
-        ncx = NiceCXNetwork()
-        ncx.set_name('Test Name')
+        cx_network = NiceCXNetwork()
+        cx_network.set_name('Test Name')
 
         a, b, c, d, e = [
-            ncx.create_node(node_name=letter)
+            cx_network.create_node(node_name=letter)
             for letter in 'ABCDE'
         ]
 
-        e1 = ncx.create_edge(
+        e1 = cx_network.create_edge(
             edge_source=a,
             edge_target=b,
         )
 
-        e2 = ncx.create_edge(
+        e2 = cx_network.create_edge(
             edge_source=b,
             edge_target=c,
             edge_interaction='increases',
         )
 
         c1 = CitationElement(id=0, title='Hi')
-        ncx.add_citation(c1)
+        cx_network.add_citation(c1)
 
         s1 = SupportElement(id=0, text='Hi')
-        ncx.add_support(s1)
+        cx_network.add_support(s1)
 
-        ncx.add_node_attribute(property_of=a, name='Color', values='Red')
-        ncx.add_node_attribute(property_of=b, name='Color', values='Red')
-        ncx.add_node_attribute(property_of=c, name='Color', values='Red')
-        ncx.add_node_attribute(property_of=d, name='Color', values='Blue')
-        ncx.add_node_attribute(property_of=e, name='Color', values='Blue')
+        cx_network.add_node_attribute(property_of=a, name='Color', values='Red')
+        cx_network.add_node_attribute(property_of=b, name='Color', values='Red')
+        cx_network.add_node_attribute(property_of=c, name='Color', values='Red')
+        cx_network.add_node_attribute(property_of=d, name='Color', values='Blue')
+        cx_network.add_node_attribute(property_of=e, name='Color', values='Blue')
 
-        ncx.add_edge_attribute(property_of=e1, name='Color', values='Green')
-        ncx.add_edge_attribute(property_of=e2, name='Color', values='Purple')
+        cx_network.add_edge_attribute(property_of=e1, name='Color', values='Green')
+        cx_network.add_edge_attribute(property_of=e2, name='Color', values='Purple')
 
-        ncx.add_edge_citations(edge_id=e1, citation_id=c1.get_id())
+        cx_network.add_edge_citations(edge_id=e1, citation_id=c1.get_id())
 
         edge_support_element_1 = {CX_CONSTANTS.PROPERTY_OF: [e1], CX_CONSTANTS.SUPPORTS: [c1.get_id()]}
-        ncx.add_edge_supports(edge_supports_element=edge_support_element_1)
+        cx_network.add_edge_supports(edge_supports_element=edge_support_element_1)
 
-        cls.cx_json = json.loads(json.dumps(ncx.to_cx()))
+        cls.cx_json = json.loads(json.dumps(cx_network.to_cx()))
         print(json.dumps(cls.cx_json, indent=2))
 
     def test_export_aspect_policy(self):
